b7c2fcddd1af5a507b50d5b78582f04c996273da
Angel Alanis <a4alaniz@gmail.com>
2019-07-17 01:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function happyHolidays() {
   console.log("Happy Holidays!")
 }
 happyHolidays()
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+happyHolidaysTo('Juan')
+
+function happyHolidayTo(holiday, name) {
+    console.log(`Happy ${holiday}, ${name}!`)
+}
+happyHolidayTo("Christmas", "Juan")
