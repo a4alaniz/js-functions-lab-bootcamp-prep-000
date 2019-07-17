de48f0271e7a271b4f9ae40a0d2363f6775e37b2
Angel Alanis <a4alaniz@gmail.com>
2019-07-17 02:01:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
+function happyHolidayTo(holiday) {
+  return "Happy ${holiday}, you!"
+}
+function holidayCountdown('holiday', 'days') {
+  return "It\'s ${days} days until ${holiday}!"
+}
