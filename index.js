33fb28128dfec440e2a294d789493461371a35af
Angel Alanis <a4alaniz@gmail.com>
2019-07-17 01:57:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// write your code below!
 function happyHolidays() {
-  console.log("Happy Holidays!")
+  return "Happy holidays!"
 }
-happyHolidays()
-
-function happyHolidaysTo(name) {
-  console.log(`Happy holidays, ${name}!`)
-}
-happyHolidaysTo('Juan')
-
-function happyHolidayTo(holiday, name) {
-    console.log(`Happy ${holiday}, ${name}!`)
-}
-happyHolidayTo("Christmas", "Juan")
-
-function holidayCountdown(days, holiday) {
-    console.log(`It's ${days} days until ${holiday}!`)
-}
-holidayCountdown("6", "Halloween")
